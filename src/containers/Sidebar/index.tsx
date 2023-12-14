dd6b788580eacb07d5519388a0d10d9008d11d01
Nelson Carvalho <nelsonfilth@hotmail.com>
2023-12-14 09:03:09 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/nordicmanx.png" />
+    <Titulo fontSize={17}>Nelson Carvalho</Titulo>
+  </aside>
+)
+
+export default Sidebar
